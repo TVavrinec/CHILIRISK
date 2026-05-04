19418c48f23c77a338c18a8b334b4cd248cfa10e
TVavrinec <tomtomvav@sezna.cz>
2026-05-04 21:11:21 +0200

    Instruction note

--- RISK-V/SOURCES/Instruction_Cache.vhd
+++ RISK-V/SOURCES/Instruction_Cache.vhd
@@ -12,7 +12,7 @@ entity Instruction_Cache is
             w_data  : in STD_LOGIC_VECTOR (31 downto 0);
            
             JUMP_F  : in STD_LOGIC;
-            PC_RST     : in STD_LOGIC;
+            PC_RST  : in STD_LOGIC;
             PC_SET  : in UNSIGNED(31 downto 0);
             
             --OUTPUTS
@@ -29,23 +29,22 @@ architecture Behavioral of Instruction_Cache is
     type mem_type is array (0 to 9215) of std_logic_vector(31 downto 0);
     signal mem : mem_type := (
 -- main (0x1CC)
-    115 => x"00A00713",
-    116 => x"2FAF07B7",
-    117 => x"07F78793",
-    118 => x"00000013",
-    119 => x"FFF78793",
-    120 => x"FE079CE3",
-    121 => x"FFF70713",
-    122 => x"FE071AE3",
-    123 => x"00008067",
-
+    115 => x"00A00713", -- addi -- rd(01110)(14)(0x0E) = rs1(0) + imm(10100000000)(1280)(0x500)
+    116 => x"02FAF7B7", -- lui  -- rd(01111)(15)(0x0F) = imm(0010111110101111000000000000)(49999872)(0x2FAF000)
+    117 => x"07F78793", -- addi -- rd(01111)(15)(0x0F) = rs1(01111)(15)(0x0F) + imm(01111111)(0x07F)
+    118 => x"00000013", -- addi -- rd(00000)(00)(0x00) = rs1(0) + imm(0) 
+    119 => x"FFF78793", -- addi -- rd(01111)(15)(0x0F) = rs1(01111)(15)(0x0F) + imm(111111111111)(0xFFF)
+    120 => x"FE079CE3", -- bne  -- if(rs1(01111)(15)(0x0F) != rs2(0)) PC += imm(0100111111111)(0x9FF)
+    121 => x"FFF70713", -- addi -- rd(01110)(14)(0x0E) = rs1(01110)(14)(0x0E) + imm(111111111111)(0xFFF)
+    122 => x"FE0714E3", -- bne  -- if(rs1(01110)(9)(0x09) != rs2(0)) PC += imm(111111110100)(0xFF4)
+    123 => x"00008067", -- jalr -- rd(0) = PC+4; PC = rs1(1) + imm(0)
 -- delay_sec (0x1F0)
-    124 => x"2FAF07B7",
-    125 => x"07F78793",
-    126 => x"00000013",
-    127 => x"FFF78793",
-    128 => x"FE0796E3",
-    129 => x"00008067",
+    124 => x"02FAF7B7", -- lui  -- rd(01111)(15)(0x0F) = imm(0010111110101111000000000000)(49999872)(0x2FAF000)
+    125 => x"07F78793", -- addi -- rd(01111)(15)(0x0F) = rs1(01111)(15)(0x0F) + imm(01111111)(0x07F)
+    126 => x"00000013", -- addi -- rd(00000)(00)(0x00) = rs1(0) + imm(0) 
+    127 => x"FFF78793", -- addi -- rd(01111)(15)(0x0F) = rs1(01111)(15)(0x0F) + imm(111111111111)(0xFFF)
+    128 => x"FE0796E3", -- bne  -- if(rs1(01111)(15)(0x0F) != rs2(0)) PC += imm(111111110110)(0xFF6)
+    129 => x"00008067", -- jalr -- rd(0) = PC+4; PC = rs1(1) + imm(0)
 
     others => (others => '0')
     );
